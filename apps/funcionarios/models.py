f47cd53871a13d5a6447fbfb3866a4775f51103e
SouzaMatheus91 <souzamanu2017@gmail.com>
2024-05-05 19:46:42 -0300

    criacao models documentos e registro_hora_extra

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamentos
 
 # Create your models here.
 class Funcionario(models.Model):
